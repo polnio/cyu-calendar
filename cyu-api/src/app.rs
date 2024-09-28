7c500425577c7c0aa58d77301c70bf51cf1c8a5a
Po Co <polniolino@gmail.com>
2024-09-28 20:21:11 +0200

    Add handlebars

--- cyu-api/src/app.rs
+++ cyu-api/src/app.rs
@@ -1,15 +1,30 @@
 use axum::extract::FromRef;
 use cyu_fetcher::Fetcher;
+use handlebars::Handlebars;
+use std::{path::PathBuf, sync::Arc};
+
+pub type TemplateEngine = Arc<Handlebars<'static>>;
 
 #[derive(Clone)]
 pub struct App {
     requester: Fetcher,
+    template_engine: TemplateEngine,
 }
 
 impl App {
     pub fn new() -> Self {
+        let mut handlebars = Handlebars::new();
+        let hbs_path = PathBuf::from(env!("CARGO_PKG_NAME")).join("views");
+        handlebars
+            .register_template_file("layout", hbs_path.join("layout.hbs"))
+            .expect("Failed to register layout template");
+        handlebars
+            .register_templates_directory(hbs_path.join("pages"), Default::default())
+            .expect("Failed to register pages templates");
+        handlebars.set_dev_mode(true);
         Self {
             requester: Fetcher::new(),
+            template_engine: handlebars.into(),
         }
     }
 }
@@ -17,5 +32,11 @@ impl App {
 impl FromRef<App> for Fetcher {
     fn from_ref(app: &App) -> Self {
         app.requester.clone()
+    }
+}
+
+impl FromRef<App> for TemplateEngine {
+    fn from_ref(app: &App) -> Self {
+        app.template_engine.clone()
     }
 }
